5273fac14258f2851ef3c9eb28a2c4e08e80c31b
Arnaud <mrandriamihaja2@gmail.com>
2020-10-21 11:32:19 +0300

    Hello Word

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text("Hello Word",
+            style: TextStyle(
+              fontSize: 22
+            ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
